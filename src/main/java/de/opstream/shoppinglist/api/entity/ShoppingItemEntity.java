b743a0655e8d50092725f957a0a63acb2b6be695
Wolfgang Liegel <wolfgang@opstream.de>
2024-02-13 19:57:45 +0100

    Changed to file based database to add datasource in IntelliJ

--- src/main/java/de/opstream/shoppinglist/api/entity/ShoppingItemEntity.java
+++ src/main/java/de/opstream/shoppinglist/api/entity/ShoppingItemEntity.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "shopping_item")
+@Table(name = "SHOPPING_ITEM")
 public class ShoppingItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
