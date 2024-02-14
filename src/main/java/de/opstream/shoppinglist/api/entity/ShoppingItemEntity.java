32781666140bff602f5f58af47110643a198fa18
Wolfgang Liegel <wolfgang@opstream.de>
2024-02-14 01:38:55 +0100

    Added basic validation to Entity

--- src/main/java/de/opstream/shoppinglist/api/entity/ShoppingItemEntity.java
+++ src/main/java/de/opstream/shoppinglist/api/entity/ShoppingItemEntity.java
@@ -1,6 +1,7 @@
 package de.opstream.shoppinglist.api.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,10 @@ public class ShoppingItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
+    @NotBlank
     public String name;
+    @NotBlank
     public String category;
     public String description;
     public boolean active = false;
